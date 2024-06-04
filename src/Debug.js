e6c4c0766f99db4588e75aa6033119921d324ccd
Francesco Michelini <hello@francescomichelini.com>
2024-06-04 19:54:03 +0200

    Update packages

--- src/Debug.js
+++ src/Debug.js
@@ -30,7 +30,7 @@ export class Debug {
       background: { r: 18, g: 18, b: 18 }
     }
 
-    folder.addInput(params, 'background', { label: 'Background Color' }).on('change', e => {
+    folder.addBinding(params, 'background', { label: 'Background Color' }).on('change', e => {
       this.app.renderer.setClearColor(new Color(e.value.r / 255, e.value.g / 255, e.value.b / 255))
     })
   }
@@ -51,17 +51,17 @@ export class Debug {
 
     this.#createColorControl(mesh.material, folder)
 
-    folder.addInput(mesh.material, 'metalness', { label: 'Metallic', min: 0, max: 1 })
-    folder.addInput(mesh.material, 'roughness', { label: 'Roughness', min: 0, max: 1 })
+    folder.addBinding(mesh.material, 'metalness', { label: 'Metallic', min: 0, max: 1 })
+    folder.addBinding(mesh.material, 'roughness', { label: 'Roughness', min: 0, max: 1 })
   }
 
   #createShadedBoxConfig() {
     const folder = this.pane.addFolder({ title: 'Shaded Box' })
     const mesh = this.app.shadedBox
 
-    folder.addInput(mesh.scale, 'x', { label: 'Width', min: 0.1, max: 4 })
-    folder.addInput(mesh.scale, 'y', { label: 'Height', min: 0.1, max: 4 })
-    folder.addInput(mesh.scale, 'z', { label: 'Depth', min: 0.1, max: 4 })
+    folder.addBinding(mesh.scale, 'x', { label: 'Width', min: 0.1, max: 4 })
+    folder.addBinding(mesh.scale, 'y', { label: 'Height', min: 0.1, max: 4 })
+    folder.addBinding(mesh.scale, 'z', { label: 'Depth', min: 0.1, max: 4 })
   }
 
   #createLightConfig() {
@@ -69,7 +69,7 @@ export class Debug {
 
     this.#createColorControl(this.app.pointLight, folder)
 
-    folder.addInput(this.app.pointLight, 'intensity', { label: 'Intensity', min: 0, max: 1000 })
+    folder.addBinding(this.app.pointLight, 'intensity', { label: 'Intensity', min: 0, max: 1000 })
   }
 
   /**
@@ -82,7 +82,7 @@ export class Debug {
     const baseColor255 = obj.color.clone().multiplyScalar(255)
     const params = { color: { r: baseColor255.r, g: baseColor255.g, b: baseColor255.b } }
 
-    folder.addInput(params, 'color', { label: 'Color' }).on('change', e => {
+    folder.addBinding(params, 'color', { label: 'Color' }).on('change', e => {
       obj.color.setRGB(e.value.r, e.value.g, e.value.b).multiplyScalar(1 / 255)
     })
   }
