0acdc118ffe266972a1b330b97dca1d7644259f3
Francesco Michelini <kekkorider@users.noreply.github.com>
2026-08-04 15:37:43 +0200

    Add HDRLoader and EXRLoader

--- src/webgl/modules/AssetLoader.ts
+++ src/webgl/modules/AssetLoader.ts
@@ -1,20 +1,28 @@
-import { LoadingManager, TextureLoader, type Texture } from 'three/webgpu'
+import { LoadingManager, TextureLoader, type Texture, type DataTexture } from 'three/webgpu'
 import { GLTFLoader, type GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { HDRLoader } from 'three/examples/jsm/loaders/HDRLoader.js'
+import { EXRLoader } from 'three/addons/loaders/EXRLoader.js'
 import { ContextModule } from "three-start"
 
 export class AssetLoaderModule extends ContextModule {
   private loadingManager: LoadingManager | null = null
   private gltfLoader: GLTFLoader | null = null
   private textureLoader: TextureLoader | null = null
+  private hdrLoader: HDRLoader | null = null
+  private exrLoader: EXRLoader | null = null
 
   private models: Map<string, GLTF> = new Map()
   private textures: Map<string, Texture> = new Map()
+  private hdrs: Map<string, Texture> = new Map()
+  private exrs: Map<string, Texture> = new Map()
 
   onAwake() {
     this.createLoadingManager()
     this.createTextureLoader()
     this.createGltfLoader()
+    this.createHdrLoader()
+    this.createExrLoader()
   }
 
   createLoadingManager(): void {
@@ -36,6 +44,65 @@ export class AssetLoaderModule extends ContextModule {
     const dracoLoader = new DRACOLoader()
     dracoLoader.setDecoderPath('/draco/')
     this.gltfLoader.setDRACOLoader(dracoLoader)
+  }
+
+  createHdrLoader(): void {
+    this.hdrLoader = new HDRLoader(this.loadingManager as LoadingManager)
+  }
+
+  createExrLoader(): void {
+    this.exrLoader = new EXRLoader(this.loadingManager as LoadingManager)
+  }
+
+  /**
+   * Load a single HDR texture or an array of HDR textures.
+   *
+   * @param resources Single URL or array of URLs of the model(s) to load.
+   */
+  loadHDR(resources: string): Promise<DataTexture>
+  loadHDR(resources: string[]): Promise<DataTexture[]>
+  async loadHDR(resources: string | string[]): Promise<DataTexture | DataTexture[]> {
+    if (Array.isArray(resources)) {
+      return Promise.all(resources.map(url => this.#loadHDR(url)) as Promise<DataTexture>[])
+    }
+    return this.#loadHDR(resources) as Promise<DataTexture>
+  }
+
+  /**
+   * Load a single texture.
+   *
+   * @param url The URL of the texture to load
+   */
+  #loadHDR(url: string): Promise<DataTexture> {
+    return new Promise((resolve, reject) => {
+      this.hdrLoader!.load(url, (texture) => {
+        this.hdrs.set(generateAssetName(url), texture)
+        return resolve(texture)
+      }, undefined, reject)
+    })
+  }
+
+  /**
+   * Load a single HDR texture or an array of HDR textures.
+   *
+   * @param resources Single URL or array of URLs of the model(s) to load.
+   */
+  loadEXR(resources: string): Promise<DataTexture>
+  loadEXR(resources: string[]): Promise<DataTexture[]>
+  async loadEXR(resources: string | string[]): Promise<DataTexture | DataTexture[]> {
+    if (Array.isArray(resources)) {
+      return Promise.all(resources.map(url => this.#loadEXR(url)) as Promise<DataTexture>[])
+    }
+    return this.#loadEXR(resources) as Promise<DataTexture>
+  }
+
+  #loadEXR(url: string): Promise<Texture> {
+    return new Promise((resolve, reject) => {
+      this.exrLoader!.load(url, (texture) => {
+        this.exrs.set(generateAssetName(url), texture)
+        return resolve(texture)
+      }, undefined, reject)
+    })
   }
 
   /**
@@ -101,6 +168,24 @@ export class AssetLoaderModule extends ContextModule {
    */
   getTexture(name: string): Texture | undefined {
     return this.textures.get(name)
+  }
+
+  /**
+   * Get a HDR texture by name.
+   *
+   * @param name The name of the HDR texture to get
+   */
+  getHDR(name: string): DataTexture | undefined {
+    return this.hdrs.get(name) as DataTexture | undefined
+  }
+
+  /**
+   * Get a EXR texture by name.
+   *
+   * @param name The name of the EXR texture to get
+   */
+  getEXR(name: string): DataTexture | undefined {
+    return this.exrs.get(name) as DataTexture | undefined
   }
 
   /**
